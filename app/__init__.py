5c5018e5d841cd018c3bfb52149e89acbea4b9e7
hoangvythptlhp0710 <hoangvytb7891@gmail.com>
2023-06-14 16:19:44 +0700

    change a few in auth

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,13 @@
+# app/__init__.py
+
+# third-party imports
 from flask import Flask
 from flask_bootstrap import Bootstrap
+from flask_login import LoginManager
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager
+
+# local imports
 from config import app_config
 
 db = SQLAlchemy()
@@ -13,23 +18,15 @@ def create_app(config_name):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_object(app_config[config_name])
     app.config.from_pyfile('config.py')
-    db.init_app(app)
-    # v1
-    # @app.route('/')
-    # def hello_world():
-    #     return 'Hello, World!'
-
-    # v2
-    login_manager.init_app(app)
-    login_manager.login_message = "You must log in to access to the webpage"
-    login_manager.login_view = "auth.login"
-
-    # v3
-    migrate = Migrate(app, db)
 
     Bootstrap(app)
-    from app import models
+    db.init_app(app)
+    login_manager.init_app(app)
+    login_manager.login_message = "You must be logged in to access this page."
+    login_manager.login_view = "auth.login"
+    migrate = Migrate(app, db)
 
+    from app import models
 
     from .admin import admin as admin_blueprint
     app.register_blueprint(admin_blueprint, url_prefix='/admin')
